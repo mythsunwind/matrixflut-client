92f048e403f33f5395be0caef30ae2c98a2ae5e5
mythsunwind <sven@wire.com>
2021-01-14 23:05:21 +0100

    New feature for clearMatrix example to clean only parts of screen

--- clearMatrix.py
+++ clearMatrix.py
@@ -1,6 +1,21 @@
 from matrixflut import Client
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-x", type=int, default=0)
+    parser.add_argument("-y", type=int, default=0)
+    parser.add_argument("-width", type=int, default=0)
+    parser.add_argument("-height", type=int, default=0)
+    args = parser.parse_args()
+
     client = Client("192.168.178.48", "1234")
-    client.clearMatrix()
+
+    if args.width == 0:
+        client.clearMatrix()
+    else:
+        for x in range(args.x, args.x + args.width):
+            for y in range(args.y, args.y + args.height):
+                client.sendPixel(x, y, "000000ff")
+
     client.close()
